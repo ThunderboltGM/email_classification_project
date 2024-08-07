904a709753a901ee10525fe2b60ff574e7ae3928
Gaurav Malik <malikgaurav868@gmail.com>
2024-08-07 17:17:38 +0530

    completed project without database and styling

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request,render_template,url_for
+import joblib
+import sqlite3
+import re
+
+mnb = joblib.load('./models/multinomialNB.lb')
+bnb = joblib.load('./models/bernoulliNB.lb')
+countVectorizer = joblib.load('./models/countvectorizer.lb')
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/project')
+def project():
+    return render_template('project.html')
+
+@app.route('/prediction',methods=['POST','GET'])
+def prediction():
+    if request.method == 'POST':
+        messege = request.form['messege']
+        messege = messege.lower()
+        final_messege = re.sub("[^a-zA-Z ]","",messege)
+        final_messege_array = countVectorizer.transform([final_messege]).toarray()
+
+        label = {1:'HAM!',0:'SPAM!'}
+
+        prediction = bnb.predict(final_messege_array)[0]
+
+        prediction = label[prediction]
+
+        return render_template('result.html',output = str(prediction))
+
+if __name__=="__main__":
+    app.run(debug=True)
